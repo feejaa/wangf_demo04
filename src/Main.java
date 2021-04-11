c6e2dcccf95455c74879e96d3c5d875283b5622f
feejaa <8256226122@qq.com>
2021-04-11 21:50:46 +0800

    m提交代码

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     //测试Git
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("再加一个");
+        System.out.println("再加一个王飞");
     }
+
 }
